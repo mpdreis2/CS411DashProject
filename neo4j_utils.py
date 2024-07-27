14af08ae1340e9604c61c6f16bc168f28f888c30
Mike Dreis <mikedreis@Mikes-MacBook-Pro.local>
2024-07-27 09:05:34 -0500

    now have neo4j database connection working with new password

--- neo4j_utils.py
+++ neo4j_utils.py
@@ -1,6 +1,6 @@
 from neo4j import GraphDatabase
 address="neo4j://localhost:7687"
-auth=('neo4j', "neo4j")
+auth=('neo4j', "ilovecs411")
 driver = GraphDatabase.driver(address, auth=auth)
 #password may be ilovecs411
 if __name__ == "__main__":
